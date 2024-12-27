f0e3df635410ed11b32001f4a600d8fadabef2c3
1dans <idansuwu@gmail.com>
2024-12-27 17:30:22 +0200

    task 2 done

--- functions_hw1.cpp
+++ functions_hw1.cpp
@@ -4,30 +4,47 @@ using namespace std;
 // void minMax(int* ptr, int size); // #1
 void fillArray(int* ptr, int size);
 void displayArray(int* ptr, int size);
+// void negPosZero(int* ptr, int size); // #2
 
 int main()
 {
 	srand(time(nullptr));
-	int size = 3;
+	// int size = 3; // #1
+	int size = 10; // #3
+	/*cout << "Enter the size of the array: ";
+	cin >> size;*/ //#2
 	int* ptr = new int[size];
 	fillArray(ptr, size);
 	displayArray(ptr, size);
+	// negPosZero(ptr, size); //#2
 	// minMax(ptr, size); #1
 
 }
 void fillArray(int* ptr, int size)
 {
 	for (int i = 0; i < size; ++i) {
-		ptr[i] = 1 + rand() % 10;
+		ptr[i] = -5 + rand() % 11;
 	}
 
 }
-void displayArray(int* ptr, int size) {
+void displayArray(int* ptr, int size) 
+{
 	for (int i = 0; i < size; ++i) {
 		cout << ptr[i] << ' ';
 	}
 	cout << endl;
 }
+
+//void negPosZero(int* ptr, int size) // #2
+//{
+//	int neg = 0, pos = 0, zero = 0; 
+//	for (int i = 0; i < size; ++i) {
+//		if (ptr[i] < 0) neg++;
+//		else if (ptr[i] > 0) pos++;
+//		else zero++;
+//	}
+//	cout << "Negative nums: " << neg << ". Positive nums: " << pos << ". Zeros: " << zero;
+//}
 
 //void minMax(int* ptr, int size) // #1
 //{
