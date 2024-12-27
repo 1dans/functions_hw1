232ddee8e7a000eddbdee871355494e5068e7fd2
1dans <idansuwu@gmail.com>
2024-12-27 17:22:25 +0200

    task 1 done

--- functions_hw1.cpp
+++ functions_hw1.cpp
@@ -1,20 +1,40 @@
-// functions_hw1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std; 
+
+// void minMax(int* ptr, int size); // #1
+void fillArray(int* ptr, int size);
+void displayArray(int* ptr, int size);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(nullptr));
+	int size = 3;
+	int* ptr = new int[size];
+	fillArray(ptr, size);
+	displayArray(ptr, size);
+	// minMax(ptr, size); #1
+
+}
+void fillArray(int* ptr, int size)
+{
+	for (int i = 0; i < size; ++i) {
+		ptr[i] = 1 + rand() % 10;
+	}
+
+}
+void displayArray(int* ptr, int size) {
+	for (int i = 0; i < size; ++i) {
+		cout << ptr[i] << ' ';
+	}
+	cout << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//void minMax(int* ptr, int size) // #1
+//{
+//	int min = 11, max=0;
+//	for (int i = 0; i < size; ++i) {
+//		if (ptr[i] >= max) max = ptr[i];
+//		if (ptr[i] <= min) min = ptr[i];
+//	}
+//	cout << "The max num is " << max << ". The min num is " << min;
+//}
